ca21f8c681419fea35ba287f722ae1185d3cf1c9
Robert Bisewski <contact@ibiscybernetics.com>
2019-09-12 13:10:19 +0000

    backup at timestamp: - Thu Sep 12 13:10:19 UTC 2019

--- ansible/hourly_backups.sh
+++ ansible/hourly_backups.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-cd /git/devops_practice_upfeat && git pull
+cd /git/devops_practice_upfeat
+git pull
+git config --global user.email "contact@ibiscybernetics.com"
+git config --global user.name "Robert Bisewski"
 cp /database/users.sqlcipher /git/devops_practice_upfeat/app/database/
-cd /git/devops_practice_upfeat && git push
+cd /git/devops_practice_upfeat
+git add *
+git commit -m "backup at timestamp: "
+git push
