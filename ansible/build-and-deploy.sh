9c9c28ef198fb27ef19df501044b2babc5d7ab2e
Robert Bisewski <contact@ibiscybernetics.com>
2019-09-11 22:55:32 -0500

    adjusting deployment script and compose

--- ansible/build-and-deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker-compose -f /git/devops_practice_upfeat/docker-compose.yml down
-docker build -t upfeat-frontend:latest /git/devops_practice_upfeat/frontend/
-docker-compose -f /git/devops_practice_upfeat/docker-compose.yml up -d
